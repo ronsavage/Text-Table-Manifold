5eca73c441a7157bc52a6966e707f5107a379a42
ronsavage <ron@savage.net.au>
2015-01-28 17:09:00 +1100

    Clean up handling of empty and undef

--- lib/Text/Table/Manifold.pm
+++ lib/Text/Table/Manifold.pm
@@ -7,25 +7,28 @@ use open     qw(:std :utf8); # Undeclared streams in UTF-8.
 
 use Const::Exporter constants =>
 [
-	# Values for style.
+	# Values for style().
 
-	as_boxed       =>  0, # The default.
-	as_github      =>  1,
+	as_boxed       => 0, # The default.
+	as_github      => 1,
 
-	# Values of centering.
+	# Values of alignment().
 
-	justify_left   =>  0,
-	justify_center =>  1,
-	justify_right  =>  2,
+	justify_left   => 0,
+	justify_center => 1,
+	justify_right  => 2,
 
 	# Values for handling missing data.
 
-	undef_as_empty =>  0,
-	undef_as_minus =>  1,
-	undef_as_text  =>  2, # 'undef'.
-	empty_as_empty =>  4,
-	empty_as_minus =>  8,
-	empty_as_text  => 16, # 'empty'.
+	empty_as_empty => 0,
+	empty_as_minus => 1,
+	empty_as_text  => 2, # 'empty'.
+	empty_as_undef => 3,
+
+	undef_as_empty => 0,
+	undef_as_minus => 1,
+	undef_as_text  => 2, # 'undef'.
+	undef_as_undef => 3,
 ];
 
 use List::AllUtils 'max';
@@ -36,7 +39,7 @@ use Types::Standard qw/Any ArrayRef HashRef Int Str/;
 
 use Unicode::GCString;
 
-has align =>
+has alignment =>
 (
 	default  => sub{return justify_center},
 	is       => 'rw',
@@ -60,15 +63,23 @@ has escapes =>
 	required => 0,
 );
 
-has headers =>
+has handle_empty =>
 (
-	default  => sub{return []},
+	default  => sub{return 0},
 	is       => 'rw',
-	isa      => ArrayRef,
+	isa      => Int,
 	required => 0,
 );
 
-has header_widths =>
+has handle_undef =>
+(
+	default  => sub{return 0},
+	is       => 'rw',
+	isa      => Int,
+	required => 0,
+);
+
+has headers =>
 (
 	default  => sub{return []},
 	is       => 'rw',
@@ -115,6 +126,7 @@ our $VERSION = '1.00';
 sub align_center
 {
 	my($self, $s, $width, $padding) = @_;
+	$s           ||= '';
 	my($s_width) = Unicode::GCString -> new($s) -> chars;
 	my($left)    = int( ($width - $s_width) / 2);
 	my($right)   = $width - $s_width - $left;
@@ -128,7 +140,8 @@ sub align_center
 sub align_left
 {
 	my($self, $s, $width, $padding) = @_;
-	my($s_width) = Unicode::GCString -> new($s) -> chars;
+	$s           ||= '';
+	my($s_width) = Unicode::GCString -> new($s || '') -> chars;
 	my($left)    = $width - $s_width;
 
 	return (' ' x ($left + $padding) ) . $s . ' ';
@@ -140,7 +153,8 @@ sub align_left
 sub align_right
 {
 	my($self, $s, $width, $padding) = @_;
-	my($s_width) = Unicode::GCString -> new($s) -> chars;
+	$s           ||= '';
+	my($s_width) = Unicode::GCString -> new($s || '') -> chars;
 	my($right)   = $width - $s_width;
 
 	return ' ' . $s . (' ' x ($right + $padding) );
@@ -149,9 +163,56 @@ sub align_right
 
 # ------------------------------------------------
 
+sub _clean_data
+{
+	my($self, $headers, $data) = @_;
+
+	for my $column (0 .. $#$headers)
+	{
+		$$headers[$column] = defined($$headers[$column]) ? $$headers[$column] : '-';
+	}
+
+	my($empty) = $self -> handle_empty;
+	my($undef) = $self -> handle_undef;
+
+	my($s);
+
+	for my $row (0 .. $#$data)
+	{
+		for my $column (0 .. $#{$$data[$row]})
+		{
+			$s = $$data[$row][$column];
+			$s = defined($s)
+					? (length($s) == 0) # Unicode::GCString should not be necessary here.
+						? ($empty & empty_as_minus)
+							? '-'
+							: ($empty & empty_as_text)
+								? 'empty'
+								: ($empty & empty_as_undef)
+									? undef
+									: $s # No need to check to empty_as_empty here!
+						: $s
+					: ($undef & undef_as_empty)
+							? ''
+							: ($undef & undef_as_minus)
+								? '-'
+								: ($undef & undef_as_text)
+									? 'undef'
+									: $s; # No need to check for undef_as_undef here!
+
+			$$data[$row][$column] = $s;
+		}
+	}
+
+} # End of _clean_data.
+
+# ------------------------------------------------
+
 sub gather_statistics
 {
 	my($self, $headers, $data) = @_;
+
+	$self -> _clean_data($headers, $data);
 
 	my($column_count);
 
@@ -159,26 +220,25 @@ sub gather_statistics
 	{
 		$column_count = $#{$$data[$row]};
 
-		die "Error: # of data columns (@{[$column_count]}) != # of header columns (@{[$#$headers]})\n" if ($column_count != $#$headers);
+		die "Error: # of data columns (@{[$column_count]}) in row @{[$row + 1]} != # of header columns (@{[$#$headers]})\n" if ($column_count != $#$headers);
 	}
 
-	my(@header_widths);
+	my(@column);
+	my($header_width);
 	my(@max_widths);
 
 	for my $column (0 .. $#$headers)
 	{
-		my(@column);
+		@column = $$headers[$column];
 
 		for my $row (0 .. $#$data)
 		{
 			push @column, $$data[$row][$column];
 		}
 
-		push @header_widths, Unicode::GCString -> new($$headers[$column]) -> chars;
-		push @max_widths, max $header_widths[$#header_widths], map{Unicode::GCString -> new($_) -> chars} @column;
+		push @max_widths, max map{Unicode::GCString -> new($_ || '') -> chars} @column;
 	}
 
-	$self -> header_widths([@header_widths]);
 	$self -> widths(\@max_widths);
 
 } # End of gather_statistics.
@@ -218,10 +278,10 @@ sub render_as_boxed
 
 	$self -> gather_statistics($headers, $data);
 
-	my($widths)    = $self -> widths;
-	my($separator) = '+' . join('+', map{'-' x ($_ + 2)} @$widths) . '+';
-	my(@output)    = $separator;
 	my($padding)   = $self -> padding;
+	my($widths)    = $self -> widths;
+	my($separator) = '+' . join('+', map{'-' x ($_ + 2 * $padding)} @$widths) . '+';
+	my(@output)    = $separator;
 
 	my(@s);
 
@@ -263,13 +323,13 @@ sub render_as_github
 
 	my(@output) = join('|', @$headers);
 
-	push @output, join('|', map{'-' x $_} @{$self -> header_widths});
+	push @output, join('|', map{'-' x $_} @{$self -> widths});
 
 	my($width);
 
 	for my $row (0 .. $#$data)
 	{
-		push @output, join('|', @{$$data[$row]});
+		push @output, join('|', map{defined($_) ? $_ : ''} @{$$data[$row]});
 	}
 
 	return [@output];
@@ -309,7 +369,8 @@ This is scripts/synopsis.pl:
 		['name', 'varchar(255)', 'not null', '', ''],
 		['upper_name', 'varchar(255)', 'not null', '', ''],
 	]);
-
+	$table -> align(justify_center);
+	$table -> padding(1);
 	$table -> style(as_boxed);
 
 	print "Style: as_boxed: \n";
@@ -414,6 +475,14 @@ Key-value pairs accepted in the parameter list (see corresponding methods for de
 
 =over 4
 
+=item o alignment => An imported constant
+
+A value for this parameter is optional.
+
+See the L</FAQ> for details.
+
+Default: justify_center.
+
 =item o data => $arrayref of arrayrefs
 
 An arrayref of arrayrefs, each one a line of data.
@@ -422,19 +491,55 @@ The # of elements in each row must match the # of elements in the C<headers> arr
 
 See the L</FAQ> for details.
 
-A value for this option is optional.
+A value for this parameter is optional.
 
 Default: [].
 
+=item o handle_empty => An imported constant
+
+A value for this parameter is optional.
+
+See the L</FAQ> for details.
+
+Default: empty_as_empty. I.e. do not transform.
+
+=item o handle_undef => An imported constant
+
+A value for this parameter is optional.
+
+See the L</FAQ> for details.
+
+Default: undef_as_empty (sic).
+
+=item o padding => $integer
+
+A value for this parameter is optional.
+
+See the L</FAQ> for details.
+
+Default: 0.
+
 =item o style => An imported constant
 
-A value for this option is optional.
+A value for this parameter is optional.
+
+See the L</FAQ> for details.
 
 Default: as_boxed.
 
 =back
 
 =head1 Methods
+
+=head2 alignment([$alignment])
+
+Here, the [] indicate an optional parameter.
+
+Returns the alignment as a constant (actually an integer).
+
+Alignment controls how many spaces are added to both sides of a cell value.
+
+See the L</FAQ#What are the constants for alignment?>.
 
 =head2 data([$arrayref])
 
@@ -448,7 +553,27 @@ All rows must have the same number of elements.
 
 Use Perl's C<undef> or '' (the empty string) for missing values.
 
-See L</missing_data([$option])> for how C<undef> and '' are handled.
+See L</handle_empty([$option])> and L</handle_undef([$option])> for how C<undef> and '' are handled.
+
+=head2 handle_empty([$option])
+
+Here, the [] indicate an optional parameter.
+
+Returns the option speciying how empty cell values ('') are being dealt with, as a constant.
+
+Controls how empty strings in cells are rendered.
+
+See the L</FAQ#What are the constants for missing data?>.
+
+=head2 handle_undef([$option])
+
+Here, the [] indicate an optional parameter.
+
+Returns the option speciying how undef cell values are being dealt with, as a constant.
+
+Controls how undefs in cells are rendered.
+
+See the L</FAQ#What are the constants for missing data?>.
 
 =head2 headers([$arrayref])
 
@@ -458,11 +583,13 @@ Returns the headers as an arrayref of strings.
 
 The structure of C<$arrayref>, if provided, must be an arrayref of strings.
 
-=head2 missing_data([$option])
+=head2 padding([$integer])
 
 Here, the [] indicate an optional parameter.
 
-Returns the missing data option.
+Returns the padding as a constant (actually an integer).
+
+Padding is the # of spaces on either side of the cell value after it has been aligned.
 
 =head2 style([$style])
 
@@ -470,7 +597,63 @@ Here, the [] indicate an optional parameter.
 
 Returns the style as a constant (actually an integer).
 
-The C<$style>, if provided, must be one of the following:
+See the L</FAQ#What are the constants for styling?>.
+
+=head1 FAQ
+
+=head2 What are the constants for alignment?
+
+The C<alignment>, if provided, must be one of the following:
+
+=over 4
+
+=item o justify_left => 0
+
+=item o justify_left => 1
+
+=item o justify_right => 2
+
+=back
+
+Note: The integer values are just here for completeness. Use the constants on their left.
+
+=head2 What are the constants for handling missing data?
+
+The C<missing data option>, if provided, must be one of the following:
+
+=over 4
+
+=item o empty_as_empty => 0
+
+Display empty cell values as the empty string.
+
+=item o empty_as_minus => 1
+
+Display empty cell values as '-'.
+
+=item o empty_as_text  => 2
+
+Display empty cell values as 'empty'.
+
+=item o undef_as_empty => 4
+
+Display undef cell values as the empty string.
+
+=item o undef_as_minus => 8
+
+Display undef cell values as '-'.
+
+=item o undef_as_text  => 16
+
+Display undef cell values as 'undef'.
+
+=back
+
+Note: The integer values are just here for completeness. Use the constants on their left.
+
+=head2 What are the constants for styling?
+
+The C<style>, if provided, must be one of the following:
 
 =over 4
 
@@ -480,8 +663,7 @@ The C<$style>, if provided, must be one of the following:
 
 =back
 
-
-=head1 FAQ
+Note: The integer values are just here for completeness. Use the constants on their left.
 
 =head2 How do I run author tests?
 
--- scripts/utf8.pl
+++ scripts/utf8.pl
@@ -1,7 +1,10 @@
 #!/usr/bin/env perl
 
 use strict;
+use utf8;
 use warnings;
+use warnings qw(FATAL utf8); # Fatalize encoding glitches.
+use open     qw(:std :utf8); # Undeclared streams in UTF-8.
 
 use Text::Table::Manifold ':constants';
 
@@ -9,19 +12,17 @@ use Text::Table::Manifold ':constants';
 
 my($table) = Text::Table::Manifold -> new;
 
-$table -> headers(['Name', 'Type', 'Null', 'Key', 'Auto increment']);
+$table -> headers(['One', 'Two', 'Three']);
 $table -> data(
 [
-	['id', 'int(11)', 'not null', 'primary key', 'auto_increment'],
-	['description', 'varchar(255)', 'not null', '', ''],
-	['name', 'varchar(255)', 'not null', '', ''],
-	['upper_name', 'varchar(255)', 'not null', '', ''],
-	[undef, '', '', '', ''],
+	['Reichwaldstraße', 'Böhme', 'ʎ ʏ ʐ ʑ ʒ ʓ ʙ ʚ'],
+	['Πηληϊάδεω Ἀχιλῆος', 'ΔΔΔΔΔΔΔΔΔΔ', 'A snowman: ☃'],
+	['Two ticks: ✔✔', undef, ''],
 ]);
-$table -> alignment(justify_center);
+
 $table -> handle_empty(empty_as_minus);
 $table -> handle_undef(undef_as_text);
-$table -> padding(1);
+$table -> padding(2);
 $table -> style(as_boxed);
 
 print "Style: as_boxed: \n";
